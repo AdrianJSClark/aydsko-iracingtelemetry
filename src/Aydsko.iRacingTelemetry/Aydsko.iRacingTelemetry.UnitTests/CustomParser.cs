7ff644cf31bee99fc490f3fe56e849f7a6c30975
Adrian Clark <adrian@ayds.com.au>
2023-08-13 01:28:30 +1000

    Tokenize Keys & Values

--- src/Aydsko.iRacingTelemetry/Aydsko.iRacingTelemetry.UnitTests/CustomParser.cs
+++ src/Aydsko.iRacingTelemetry/Aydsko.iRacingTelemetry.UnitTests/CustomParser.cs
@@ -15,26 +15,26 @@ public class CustomParser
         {
             ExpectedResult = new YamlParser.YamlToken[]
             {
-                new(0, 5, YamlParser.YamlTokenType.Literal),
+                new(0, 5, YamlParser.YamlTokenType.Key),
                 new(5, 1, YamlParser.YamlTokenType.KeySeparator),
-                new(6, 5, YamlParser.YamlTokenType.Literal),
+                new(6, 5, YamlParser.YamlTokenType.Value),
             }
         };
         yield return new("hello: world")
         {
             ExpectedResult = new YamlParser.YamlToken[]
             {
-                new(0, 5, YamlParser.YamlTokenType.Literal),
+                new(0, 5, YamlParser.YamlTokenType.Key),
                 new(5, 1, YamlParser.YamlTokenType.KeySeparator),
                 new(6, 1, YamlParser.YamlTokenType.Whitespace),
-                new(7, 5, YamlParser.YamlTokenType.Literal),
+                new(7, 5, YamlParser.YamlTokenType.Value),
             }
         };
         yield return new("hello: 'world'")
         {
             ExpectedResult = new YamlParser.YamlToken[]
             {
-                new(0, 5, YamlParser.YamlTokenType.Literal),
+                new(0, 5, YamlParser.YamlTokenType.Key),
                 new(5, 1, YamlParser.YamlTokenType.KeySeparator),
                 new(6, 1, YamlParser.YamlTokenType.Whitespace),
                 new(7, 1, YamlParser.YamlTokenType.StartQuote),
@@ -46,7 +46,7 @@ public class CustomParser
         {
             ExpectedResult = new YamlParser.YamlToken[]
             {
-                new(0, 5, YamlParser.YamlTokenType.Literal),
+                new(0, 5, YamlParser.YamlTokenType.Key),
                 new(5, 1, YamlParser.YamlTokenType.KeySeparator),
                 new(6, 1, YamlParser.YamlTokenType.Whitespace),
                 new(7, 1, YamlParser.YamlTokenType.StartQuote),
@@ -62,20 +62,20 @@ public class CustomParser
         {
             ExpectedResult = new YamlParser.YamlToken[]
             {
-                new(0, 9, YamlParser.YamlTokenType.Literal),
+                new(0, 9, YamlParser.YamlTokenType.Key),
                 new(9, 1, YamlParser.YamlTokenType.KeySeparator),
                 new(10, 2, YamlParser.YamlTokenType.NewLine),
                 new(12, 1, YamlParser.YamlTokenType.Whitespace),
-                new(13, 4, YamlParser.YamlTokenType.Literal),
+                new(13, 4, YamlParser.YamlTokenType.Key),
                 new(17,1, YamlParser.YamlTokenType.KeySeparator),
                 new(18, 1, YamlParser.YamlTokenType.Whitespace),
-                new(19,6, YamlParser.YamlTokenType.Literal),
+                new(19,6, YamlParser.YamlTokenType.Value),
                 new(25, 2, YamlParser.YamlTokenType.NewLine),
                 new(27, 1, YamlParser.YamlTokenType.Whitespace),
-                new(28, 4, YamlParser.YamlTokenType.Literal),
+                new(28, 4, YamlParser.YamlTokenType.Key),
                 new(32,1, YamlParser.YamlTokenType.KeySeparator),
                 new(33, 1, YamlParser.YamlTokenType.Whitespace),
-                new(34,6, YamlParser.YamlTokenType.Literal),
+                new(34,6, YamlParser.YamlTokenType.Value),
             }
         };
         yield return new("""
@@ -86,11 +86,11 @@ public class CustomParser
         {
             ExpectedResult = new YamlParser.YamlToken[]
             {
-                new(0, 9, YamlParser.YamlTokenType.Literal),
+                new(0, 9, YamlParser.YamlTokenType.Key),
                 new(9, 1, YamlParser.YamlTokenType.KeySeparator),
                 new(10, 2, YamlParser.YamlTokenType.NewLine),
                 new(12, 1, YamlParser.YamlTokenType.Whitespace),
-                new(13, 4, YamlParser.YamlTokenType.Literal),
+                new(13, 4, YamlParser.YamlTokenType.Key),
                 new(17,1, YamlParser.YamlTokenType.KeySeparator),
                 new(18, 1, YamlParser.YamlTokenType.Whitespace),
                 new(19, 1, YamlParser.YamlTokenType.StartQuote),
@@ -98,7 +98,7 @@ public class CustomParser
                 new(26, 1, YamlParser.YamlTokenType.EndQuote),
                 new(27, 2, YamlParser.YamlTokenType.NewLine),
                 new(29, 1, YamlParser.YamlTokenType.Whitespace),
-                new(30, 4, YamlParser.YamlTokenType.Literal),
+                new(30, 4, YamlParser.YamlTokenType.Key),
                 new(34,1, YamlParser.YamlTokenType.KeySeparator),
                 new(35, 1, YamlParser.YamlTokenType.Whitespace),
                 new(36, 1, YamlParser.YamlTokenType.StartQuote),
@@ -114,18 +114,18 @@ public class CustomParser
         {
             ExpectedResult = new YamlParser.YamlToken[]
             {
-                new(0, 4, YamlParser.YamlTokenType.Literal),
+                new(0, 4, YamlParser.YamlTokenType.Key),
                 new(4, 1, YamlParser.YamlTokenType.KeySeparator),
                 new(5, 2, YamlParser.YamlTokenType.NewLine),
                 new(7, 1, YamlParser.YamlTokenType.Whitespace),
                 new(8, 1, YamlParser.YamlTokenType.ListBullet),
                 new(9,1, YamlParser.YamlTokenType.Whitespace),
-                new(10,6,YamlParser.YamlTokenType.Literal),
+                new(10,6,YamlParser.YamlTokenType.Value),
                 new(16,2, YamlParser.YamlTokenType.NewLine),
                 new(18,1, YamlParser.YamlTokenType.Whitespace),
                 new(19, 1, YamlParser.YamlTokenType.ListBullet),
                 new(20,1, YamlParser.YamlTokenType.Whitespace),
-                new(21,6,YamlParser.YamlTokenType.Literal),
+                new(21,6,YamlParser.YamlTokenType.Value),
             }
         };
         yield return new("""
@@ -151,117 +151,96 @@ public class CustomParser
         {
             ExpectedResult = new YamlParser.YamlToken[]
             {
-                new(0, 8, YamlParser.YamlTokenType.Literal),
+                new(0, 8, YamlParser.YamlTokenType.Key),
                 new(8, 1, YamlParser.YamlTokenType.KeySeparator),
                 new(9, 2, YamlParser.YamlTokenType.NewLine), // CarSetup:
                 new(11,1, YamlParser.YamlTokenType.Whitespace),
-                new(12,11, YamlParser.YamlTokenType.Literal),
+                new(12,11, YamlParser.YamlTokenType.Key),
                 new(23,1, YamlParser.YamlTokenType.KeySeparator),
                 new(24,1, YamlParser.YamlTokenType.Whitespace),
-                new(25,1,YamlParser.YamlTokenType.Literal),
+                new(25,1,YamlParser.YamlTokenType.Value),
                 new(26,2, YamlParser.YamlTokenType.NewLine), // UpdateCount: 8
                 new(28,1, YamlParser.YamlTokenType.Whitespace),
-                new(29,9, YamlParser.YamlTokenType.Literal),
+                new(29,9, YamlParser.YamlTokenType.Key),
                 new(38,1,YamlParser.YamlTokenType.KeySeparator),
                 new(39,2, YamlParser.YamlTokenType.NewLine), // TiresAero:
                 new(41,2, YamlParser.YamlTokenType.Whitespace),
-                new(43,12, YamlParser.YamlTokenType.Literal),
+                new(43,12, YamlParser.YamlTokenType.Key),
                 new(55,1,YamlParser.YamlTokenType.KeySeparator),
                 new(56,2, YamlParser.YamlTokenType.NewLine), // TireCompound:
                 new(58,3, YamlParser.YamlTokenType.Whitespace),
-                new(61,12, YamlParser.YamlTokenType.Literal),
+                new(61,12, YamlParser.YamlTokenType.Key),
                 new(73,1,YamlParser.YamlTokenType.KeySeparator),
                 new(74,1,YamlParser.YamlTokenType.Whitespace),
-                new(75,4, YamlParser.YamlTokenType.Literal),
+                new(75,4, YamlParser.YamlTokenType.Value),
                 new(79,2, YamlParser.YamlTokenType.NewLine), // TireCompound: Soft
                 new(81,2, YamlParser.YamlTokenType.Whitespace),
-                new(83,13, YamlParser.YamlTokenType.Literal),
+                new(83,13, YamlParser.YamlTokenType.Key),
                 new(96,1,YamlParser.YamlTokenType.KeySeparator),
                 new(97,2, YamlParser.YamlTokenType.NewLine), // LeftFrontTire:
                 new(99,3,YamlParser.YamlTokenType.Whitespace),
-                new(102,16, YamlParser.YamlTokenType.Literal),
+                new(102,16, YamlParser.YamlTokenType.Key),
                 new(118,1,YamlParser.YamlTokenType.KeySeparator),
                 new(119,1, YamlParser.YamlTokenType.Whitespace),
-                new(120,5, YamlParser.YamlTokenType.Literal),
-                new(125,1,YamlParser.YamlTokenType.Whitespace),
-                new(126,3,YamlParser.YamlTokenType.Literal),
+                new(120,9, YamlParser.YamlTokenType.Value),
                 new(129,2,YamlParser.YamlTokenType.NewLine), // StartingPressure: 151.7 kPa
                 new(131,3,YamlParser.YamlTokenType.Whitespace),
-                new(134,15, YamlParser.YamlTokenType.Literal),
+                new(134,15, YamlParser.YamlTokenType.Key),
                 new(149,1,YamlParser.YamlTokenType.KeySeparator),
                 new(150,1, YamlParser.YamlTokenType.Whitespace),
-                new(151,4, YamlParser.YamlTokenType.Literal),
-                new(155,1,YamlParser.YamlTokenType.Whitespace),
-                new(156,3,YamlParser.YamlTokenType.Literal),
+                new(151,8, YamlParser.YamlTokenType.Value),
                 new(159,2,YamlParser.YamlTokenType.NewLine), // LastHotPressure: 22.8 psi
                 new(161,3,YamlParser.YamlTokenType.Whitespace),
-                new(164,12, YamlParser.YamlTokenType.Literal),
+                new(164,12, YamlParser.YamlTokenType.Key),
                 new(176,1,YamlParser.YamlTokenType.KeySeparator),
                 new(177,1, YamlParser.YamlTokenType.Whitespace),
-                new(178,5, YamlParser.YamlTokenType.Literal),
-                new(183,1,YamlParser.YamlTokenType.Whitespace),
-                new(184,5, YamlParser.YamlTokenType.Literal),
-                new(189,1,YamlParser.YamlTokenType.Whitespace),
-                new(190,4, YamlParser.YamlTokenType.Literal),
+                new(178,16, YamlParser.YamlTokenType.Value),
                 new(194,2,YamlParser.YamlTokenType.NewLine), // LastTempsOMI: 115C, 121C, 127C
                 new(196,3,YamlParser.YamlTokenType.Whitespace),
-                new(199,14, YamlParser.YamlTokenType.Literal),
+                new(199,14, YamlParser.YamlTokenType.Key),
                 new(213,1,YamlParser.YamlTokenType.KeySeparator),
                 new(214,1, YamlParser.YamlTokenType.Whitespace),
-                new(215,4, YamlParser.YamlTokenType.Literal),
-                new(219,1,YamlParser.YamlTokenType.Whitespace),
-                new(223,4, YamlParser.YamlTokenType.Literal),
-                new(224,1,YamlParser.YamlTokenType.Whitespace),
-                new(225,3, YamlParser.YamlTokenType.Literal),
+                new(215,13, YamlParser.YamlTokenType.Value),
                 new(228,2,YamlParser.YamlTokenType.NewLine), // TreadRemaining: 91%, 86%, 86%
                 new(230,1, YamlParser.YamlTokenType.Whitespace),
-                new(231,7, YamlParser.YamlTokenType.Literal),
+                new(231,7, YamlParser.YamlTokenType.Key),
                 new(238,1,YamlParser.YamlTokenType.KeySeparator),
                 new(239,2, YamlParser.YamlTokenType.NewLine), // Chassis:
                 new(241,2, YamlParser.YamlTokenType.Whitespace),
-                new(243,9, YamlParser.YamlTokenType.Literal),
+                new(243,9, YamlParser.YamlTokenType.Key),
                 new(252,1,YamlParser.YamlTokenType.KeySeparator),
                 new(253,2, YamlParser.YamlTokenType.NewLine), // LeftFront:
                 new(255,3, YamlParser.YamlTokenType.Whitespace),
-                new(258,12, YamlParser.YamlTokenType.Literal),
+                new(258,12, YamlParser.YamlTokenType.Key),
                 new(270,1,YamlParser.YamlTokenType.KeySeparator),
                 new(271,1,YamlParser.YamlTokenType.Whitespace),
-                new(272,4, YamlParser.YamlTokenType.Literal),
-                new(276,1,YamlParser.YamlTokenType.Whitespace),
-                new(277,1,YamlParser.YamlTokenType.Literal),
+                new(272,6, YamlParser.YamlTokenType.Value),
                 new(278,2, YamlParser.YamlTokenType.NewLine), // CornerWeight: 1964 N
                 new(280,3, YamlParser.YamlTokenType.Whitespace),
-                new(283,6, YamlParser.YamlTokenType.Literal),
+                new(283,6, YamlParser.YamlTokenType.Key),
                 new(289,1,YamlParser.YamlTokenType.KeySeparator),
                 new(290,1,YamlParser.YamlTokenType.Whitespace),
-                new(291,5, YamlParser.YamlTokenType.Literal),
-                new(297,1,YamlParser.YamlTokenType.Whitespace),
-                new(298,3,YamlParser.YamlTokenType.Literal),
-                new(301,2, YamlParser.YamlTokenType.NewLine), // Camber: -3.48 deg
-                new(303,1, YamlParser.YamlTokenType.Whitespace),
-                new(304,10, YamlParser.YamlTokenType.Literal),
-                new(314,1,YamlParser.YamlTokenType.KeySeparator),
-                new(315,2, YamlParser.YamlTokenType.NewLine), // DriveBrake:
-                new(317,2, YamlParser.YamlTokenType.Whitespace),
-                new(319,17, YamlParser.YamlTokenType.Literal),
-                new(336,1,YamlParser.YamlTokenType.KeySeparator),
-                new(337,2, YamlParser.YamlTokenType.NewLine), // BrakeSystemConfig:
-                new(339,3, YamlParser.YamlTokenType.Whitespace),
-                new(342,13, YamlParser.YamlTokenType.Literal),
-                new(355,1,YamlParser.YamlTokenType.KeySeparator),
-                new(356,1,YamlParser.YamlTokenType.Whitespace),
-                new(357,5, YamlParser.YamlTokenType.Literal),
-                new(362,1,YamlParser.YamlTokenType.Whitespace),
-                new(363,6,YamlParser.YamlTokenType.Literal),
-                new(369,2, YamlParser.YamlTokenType.NewLine), // BaseBrakeBias: 57.0% (BBAL)
-                new(371,3, YamlParser.YamlTokenType.Whitespace),
-                new(374,13, YamlParser.YamlTokenType.Literal),
-                new(387,1,YamlParser.YamlTokenType.KeySeparator),
-                new(388,1,YamlParser.YamlTokenType.Whitespace),
-                new(391,3, YamlParser.YamlTokenType.Literal),
-                new(394,1,YamlParser.YamlTokenType.Whitespace),
-                new(395,9,YamlParser.YamlTokenType.Literal),
-                new(404,2, YamlParser.YamlTokenType.NewLine), // FineBrakeBias: 0.0 (BB+/BB-)
+                new(291,9, YamlParser.YamlTokenType.Value),
+                new(300,2, YamlParser.YamlTokenType.NewLine), // Camber: -3.48 deg
+                new(302,1, YamlParser.YamlTokenType.Whitespace),
+                new(303,10, YamlParser.YamlTokenType.Key),
+                new(313,1,YamlParser.YamlTokenType.KeySeparator),
+                new(314,2, YamlParser.YamlTokenType.NewLine), // DriveBrake:
+                new(316,2, YamlParser.YamlTokenType.Whitespace),
+                new(318,17, YamlParser.YamlTokenType.Key),
+                new(335,1,YamlParser.YamlTokenType.KeySeparator),
+                new(336,2, YamlParser.YamlTokenType.NewLine), // BrakeSystemConfig:
+                new(338,3, YamlParser.YamlTokenType.Whitespace),
+                new(341,13, YamlParser.YamlTokenType.Key),
+                new(354,1,YamlParser.YamlTokenType.KeySeparator),
+                new(355,1,YamlParser.YamlTokenType.Whitespace),
+                new(356,12, YamlParser.YamlTokenType.Value),
+                new(368,2, YamlParser.YamlTokenType.NewLine), // BaseBrakeBias: 57.0% (BBAL)
+                new(370,3, YamlParser.YamlTokenType.Whitespace),
+                new(373,13, YamlParser.YamlTokenType.Key),
+                new(386,1,YamlParser.YamlTokenType.KeySeparator),
+                new(387,1,YamlParser.YamlTokenType.Whitespace),
+                new(388,13, YamlParser.YamlTokenType.Value), // FineBrakeBias: 0.0 (BB+/BB-)
             }
         };
     }
--- src/Aydsko.iRacingTelemetry/Aydsko.iRacingTelemetry/YamlParser.cs
+++ src/Aydsko.iRacingTelemetry/Aydsko.iRacingTelemetry/YamlParser.cs
@@ -1,6 +1,4 @@
-﻿using System.Security;
-
-namespace Aydsko.iRacingTelemetry;
+﻿namespace Aydsko.iRacingTelemetry;
 
 public static class YamlParser
 {
@@ -17,6 +15,7 @@ public static class YamlParser
         var start = 0;
         var end = 0;
         var current = YamlTokenType.Unknown;
+        var keySeparatorSeen = false;
 
         foreach (var c in input.Span)
         {
@@ -24,16 +23,17 @@ public static class YamlParser
             {
                 case '\r':
                 case '\n':
-                    if (current != YamlTokenType.NewLine && current != YamlTokenType.Unknown)
+                    if (current is not YamlTokenType.NewLine and not YamlTokenType.Unknown)
                     {
                         results.Add(new(start, end - start, current));
                         start = end;
                     }
                     current = YamlTokenType.NewLine;
                     end++;
+                    keySeparatorSeen = false;
                     continue;
 
-                case ':' when (current == YamlTokenType.QuotedString):
+                case ':' when current == YamlTokenType.QuotedString:
                     end++;
                     continue;
 
@@ -42,10 +42,11 @@ public static class YamlParser
                     start = end;
                     end++;
                     current = YamlTokenType.KeySeparator;
+                    keySeparatorSeen = true;
                     continue;
 
-                case '\'' when (current == YamlTokenType.QuotedString):
-                case '\"' when (current == YamlTokenType.QuotedString):
+                case '\'' when current == YamlTokenType.QuotedString:
+                case '\"' when current == YamlTokenType.QuotedString:
                     results.Add(new(start, end - start, current));
                     start = end;
                     end++;
@@ -60,19 +61,20 @@ public static class YamlParser
                     current = YamlTokenType.StartQuote;
                     continue;
 
-                case ' ' when (current == YamlTokenType.Whitespace || current == YamlTokenType.QuotedString):
+                case ' ' when current is YamlTokenType.Whitespace or YamlTokenType.QuotedString or YamlTokenType.Value:
                     end++;
                     continue;
 
-                case ' ' when (current == YamlTokenType.NewLine || current == YamlTokenType.ListBullet):
+                case ' ' when current is YamlTokenType.NewLine or YamlTokenType.ListBullet:
                     results.Add(new(start, end - start, current));
                     start = end;
                     end++;
+                    keySeparatorSeen = current == YamlTokenType.ListBullet;
                     current = YamlTokenType.Whitespace;
                     continue;
 
                 case ' ':
-                    if (current != YamlTokenType.Whitespace && current != YamlTokenType.Unknown)
+                    if (current is not YamlTokenType.Whitespace and not YamlTokenType.Unknown)
                     {
                         results.Add(new(start, end - start, current));
                         start = end;
@@ -81,7 +83,7 @@ public static class YamlParser
                     end++;
                     continue;
 
-                case '-' when (current == YamlTokenType.Whitespace || current == YamlTokenType.NewLine):
+                case '-' when keySeparatorSeen == false && (current == YamlTokenType.Whitespace || current == YamlTokenType.NewLine):
                     results.Add(new(start, end - start, current));
                     start = end;
                     end++;
@@ -103,13 +105,19 @@ public static class YamlParser
                     }
                     else if (current == YamlTokenType.Unknown)
                     {
-                        current = YamlTokenType.Literal;
+                        current = YamlTokenType.Key;
                     }
-                    else if (current == YamlTokenType.Whitespace || current == YamlTokenType.NewLine || current == YamlTokenType.KeySeparator)
+                    else if (current == YamlTokenType.KeySeparator)
                     {
                         results.Add(new(start, end - start, current));
                         start = end;
-                        current = YamlTokenType.Literal;
+                        current = YamlTokenType.Value;
+                    }
+                    else if (current is YamlTokenType.Whitespace or YamlTokenType.NewLine)
+                    {
+                        results.Add(new(start, end - start, current));
+                        start = end;
+                        current = keySeparatorSeen ? YamlTokenType.Value : YamlTokenType.Key;
                     }
 
                     end++;
@@ -130,9 +138,10 @@ public static class YamlParser
     public enum YamlTokenType
     {
         Unknown,
-        Literal,
+        Key,
         KeySeparator,
         Whitespace,
+        Value,
         StartQuote,
         QuotedString,
         EndQuote,
