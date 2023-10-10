3bc1b45009e2ff8d899e5f639d4c72317ceedde4
Adrian Clark <adrian@ayds.com.au>
2023-10-10 16:33:01 +1000

    Add Link for Extra Information to Track Location

--- src/Aydsko.iRacingTelemetry/Aydsko.iRacingTelemetry/TrackLocation.cs
+++ src/Aydsko.iRacingTelemetry/Aydsko.iRacingTelemetry/TrackLocation.cs
@@ -1,5 +1,6 @@
 ﻿namespace Aydsko.iRacingTelemetry;
 
+// TODO - make use of this awesome piece of information: https://forums.iracing.com/discussion/comment/421060/#Comment_421060
 public enum TrackLocation
 {
     NotInWorld = -1,
